40352a30980a26466e4bf8341666ace154b90cdb
KD <kdbhalala@gmail.com>
2026-09-16 15:51:27 +0530

    chore(formula): update Homebrew formula to v1.0.3

--- Formula/avdslim.rb
+++ Formula/avdslim.rb
@@ -1,26 +1,26 @@
 class Avdslim < Formula
   desc "Drop Android Virtual Device (AVD) RAM from ~8GB to ~1.5GB on Apple Silicon & Linux"
   homepage "https://github.com/kdbhalala/avdslim"
-  version "1.0.2"
+  version "1.0.3"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/kdbhalala/avdslim/releases/download/v1.0.2/avdslim_v1.0.2_darwin_arm64.tar.gz"
-      sha256 "670600e10d5f36d44f3d91da25496b62c940d339de2e566b6e5bd6941ad4d5c5"
+      url "https://github.com/kdbhalala/avdslim/releases/download/v1.0.3/avdslim_v1.0.3_darwin_arm64.tar.gz"
+      sha256 "8289ed854b3b88f548a90320096103ebd8a9bc18bd7d2e223ca48dc3ff936df8"
     else
-      url "https://github.com/kdbhalala/avdslim/releases/download/v1.0.2/avdslim_v1.0.2_darwin_amd64.tar.gz"
-      sha256 "7e3e55c565cb37ac13c94170752b03e139c4d8ff588aff5228d236d900499de9"
+      url "https://github.com/kdbhalala/avdslim/releases/download/v1.0.3/avdslim_v1.0.3_darwin_amd64.tar.gz"
+      sha256 "046056ae8cf66d626dfc8147b77512fbcff4952a9e73d3896281e642607be965"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/kdbhalala/avdslim/releases/download/v1.0.2/avdslim_v1.0.2_linux_arm64.tar.gz"
-      sha256 "15d834e2fa5a2e8d5d816727609f6e2e28a65432ec708b73b69aec608b050b08"
+      url "https://github.com/kdbhalala/avdslim/releases/download/v1.0.3/avdslim_v1.0.3_linux_arm64.tar.gz"
+      sha256 "1880c70c5377281b79bfc4700b2ae33cdb656cd39ec061303e9d3acb43f60d64"
     else
-      url "https://github.com/kdbhalala/avdslim/releases/download/v1.0.2/avdslim_v1.0.2_linux_amd64.tar.gz"
-      sha256 "908c67c56f910587cd4e2af9fc5be3160c5571c6c6399c15b61db8149eaf700c"
+      url "https://github.com/kdbhalala/avdslim/releases/download/v1.0.3/avdslim_v1.0.3_linux_amd64.tar.gz"
+      sha256 "2d07bbe6b15ade5be2e2f9ae84ff146039421882e7c43d5f9c88594f88280d8c"
     end
   end
 
