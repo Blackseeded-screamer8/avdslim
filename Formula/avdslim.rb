791e43e99230af33a52cb4f7d84a4274430c7648
KD <kdbhalala@gmail.com>
2026-09-16 12:26:44 +0530

    feat: add binary Homebrew formula with sha256 checksums

--- Formula/avdslim.rb
+++ Formula/avdslim.rb
@@ -1,15 +1,31 @@
 class Avdslim < Formula
   desc "Drop Android Virtual Device (AVD) RAM from ~8GB to ~1.5GB on Apple Silicon & Linux"
   homepage "https://github.com/kdbhalala/avdslim"
-  url "https://github.com/kdbhalala/avdslim/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "PLACEHOLDER_SHA256"
+  version "1.0.0"
   license "MIT"
-  head "https://github.com/kdbhalala/avdslim.git", branch: "main"
 
-  depends_on "go" => :build
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/kdbhalala/avdslim/releases/download/v1.0.0/avdslim_v1.0.0_darwin_arm64.tar.gz"
+      sha256 "70833d73e3a7ea51d49e6f01476054fe07671b8cf3655544f064a848e8ee3cd1"
+    else
+      url "https://github.com/kdbhalala/avdslim/releases/download/v1.0.0/avdslim_v1.0.0_darwin_amd64.tar.gz"
+      sha256 "ef2b12232455530d371055ab05cb1f03e07e953edc2874be642f954a28edd915"
+    end
+  end
+
+  on_linux do
+    if Hardware::CPU.arm?
+      url "https://github.com/kdbhalala/avdslim/releases/download/v1.0.0/avdslim_v1.0.0_linux_arm64.tar.gz"
+      sha256 "35da1cac9319a1dd244d623daae4f77c4799745494fec38cf4fcdce538a462bd"
+    else
+      url "https://github.com/kdbhalala/avdslim/releases/download/v1.0.0/avdslim_v1.0.0_linux_amd64.tar.gz"
+      sha256 "e6bb765d99242397c0fa52d27757dd08a51e7311f737efdd10b5fb3506c17424"
+    end
+  end
 
   def install
-    system "go", "build", *std_go_args(ldflags: "-s -w -X main.version=#{version}"), "./cmd/avdslim"
+    bin.install "avdslim"
   end
 
   test do
