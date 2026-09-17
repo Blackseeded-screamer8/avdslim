aa4577c00fb0bc8b032ef226f0dc06902c931cad
KD <kdbhalala@gmail.com>
2026-09-17 15:21:16 +0530

    chore(release): update Homebrew formula for v1.0.7

--- Formula/avdslim.rb
+++ Formula/avdslim.rb
@@ -1,26 +1,26 @@
 class Avdslim < Formula
   desc "Drop Android Virtual Device (AVD) RAM from ~8GB to ~1.5GB on Apple Silicon & Linux"
   homepage "https://github.com/kdbhalala/avdslim"
-  version "1.0.6"
+  version "1.0.7"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/kdbhalala/avdslim/releases/download/v1.0.6/avdslim_v1.0.6_darwin_arm64.tar.gz"
-      sha256 "49ab1ef5034b1077ac8e612af26345809a91ee160fe69e251f9faa2dafb5725c"
+      url "https://github.com/kdbhalala/avdslim/releases/download/v1.0.7/avdslim_v1.0.7_darwin_arm64.tar.gz"
+      sha256 "e596deacc0f8c631fa05a34355be8f537cf66e30002e8f5d0d5a738aa3364f46"
     else
-      url "https://github.com/kdbhalala/avdslim/releases/download/v1.0.6/avdslim_v1.0.6_darwin_amd64.tar.gz"
-      sha256 "dd0c67cccea0c648af3f135c3bbaa1340675939f783b3ac996f500c2ba21bc53"
+      url "https://github.com/kdbhalala/avdslim/releases/download/v1.0.7/avdslim_v1.0.7_darwin_amd64.tar.gz"
+      sha256 "fb9f54493b9f4d6a341a9bbe569d78dea574aa4d359fd5ebfb93f75037cacdc4"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/kdbhalala/avdslim/releases/download/v1.0.6/avdslim_v1.0.6_linux_arm64.tar.gz"
-      sha256 "2cdc54f60daebf9061f74fb79802464bb99b4cac2831d59e6d618f67a3335503"
+      url "https://github.com/kdbhalala/avdslim/releases/download/v1.0.7/avdslim_v1.0.7_linux_arm64.tar.gz"
+      sha256 "d84969dfb3c75e17f2754412d5cbd89a5b0f4d0d7852f41137c582f7868395cd"
     else
-      url "https://github.com/kdbhalala/avdslim/releases/download/v1.0.6/avdslim_v1.0.6_linux_amd64.tar.gz"
-      sha256 "96f260edfffb2bbe13cb284d2bcd66411e722f47f4091834cea9bc0c26d8a05d"
+      url "https://github.com/kdbhalala/avdslim/releases/download/v1.0.7/avdslim_v1.0.7_linux_amd64.tar.gz"
+      sha256 "cc6d79d82f357fa2f19dfb198195db0f10a772eb9da8fbbe2e01cc55452d17ef"
     end
   end
 
