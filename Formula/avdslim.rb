f74f8779001b203c38ba2a53acdc69acb913f6b2
KD <kdbhalala@gmail.com>
2026-09-17 17:28:05 +0530

    chore(release): update Homebrew formula for v1.0.9
    Update binary URLs and SHA-256 checksums for all four platforms.

--- Formula/avdslim.rb
+++ Formula/avdslim.rb
@@ -1,26 +1,26 @@
 class Avdslim < Formula
   desc "Drop Android Virtual Device (AVD) RAM from ~8GB to ~1.5GB on Apple Silicon & Linux"
   homepage "https://github.com/kdbhalala/avdslim"
-  version "1.0.8"
+  version "1.0.9"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/kdbhalala/avdslim/releases/download/v1.0.8/avdslim_v1.0.8_darwin_arm64.tar.gz"
-      sha256 "f36ab9aa32f8bc538942f9e09e9f0b559468a667bde075adc537c1bc9d9a7f06"
+      url "https://github.com/kdbhalala/avdslim/releases/download/v1.0.9/avdslim_v1.0.9_darwin_arm64.tar.gz"
+      sha256 "67094ba86373ee2d9d05b34b20a28ce14f6efb751c8551f7a8f3c91c6de24c04"
     else
-      url "https://github.com/kdbhalala/avdslim/releases/download/v1.0.8/avdslim_v1.0.8_darwin_amd64.tar.gz"
-      sha256 "9438911be1c647d6413bae1c00876f07e9f7f39936f86c06d01d19744ff4f415"
+      url "https://github.com/kdbhalala/avdslim/releases/download/v1.0.9/avdslim_v1.0.9_darwin_amd64.tar.gz"
+      sha256 "efeb20a1c625bac1d2cdf21e9fa5ea069ea890567b9da9344b34f43cc39a8c30"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/kdbhalala/avdslim/releases/download/v1.0.8/avdslim_v1.0.8_linux_arm64.tar.gz"
-      sha256 "dfccb91a5cf9ae1b4c143768d31e6d03eece3dcb540ef20ade55ce9100e43f05"
+      url "https://github.com/kdbhalala/avdslim/releases/download/v1.0.9/avdslim_v1.0.9_linux_arm64.tar.gz"
+      sha256 "1cd21406b3802102cf8eb7045dccefa4428fd08f6a577300ee969a2dd9319694"
     else
-      url "https://github.com/kdbhalala/avdslim/releases/download/v1.0.8/avdslim_v1.0.8_linux_amd64.tar.gz"
-      sha256 "a445d243abdd3007327867581f35b8641614de3c1f8cd904d5b52eb539898876"
+      url "https://github.com/kdbhalala/avdslim/releases/download/v1.0.9/avdslim_v1.0.9_linux_amd64.tar.gz"
+      sha256 "3ad0983780b308c59ecb433702eccb9ca8b7529844b11c2ac71b7f7a8555217b"
     end
   end
 
