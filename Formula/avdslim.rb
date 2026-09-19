0f4dd4f13842d6cb1f204c9c6f3a3d15979e89df
KD <kdbhalala@gmail.com>
2026-09-20 00:00:08 +0530

    chore(release): update Homebrew formula for v1.0.12
    Update URLs and sha256 checksums for v1.0.12 release tarballs.

--- Formula/avdslim.rb
+++ Formula/avdslim.rb
@@ -1,26 +1,26 @@
 class Avdslim < Formula
   desc "Drop Android Virtual Device (AVD) RAM from ~8GB to ~1.5GB on Apple Silicon & Linux"
   homepage "https://github.com/kdbhalala/avdslim"
-  version "1.0.11"
+  version "1.0.12"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/kdbhalala/avdslim/releases/download/v1.0.11/avdslim_v1.0.11_darwin_arm64.tar.gz"
-      sha256 "e304e6f3ff24a8f93f7e0a75edac14b33ae0870db79b5933ecdc24275b2b2c91"
+      url "https://github.com/kdbhalala/avdslim/releases/download/v1.0.12/avdslim_v1.0.12_darwin_arm64.tar.gz"
+      sha256 "0bf46f9e93c8373fd239f2990b311fe38cdbe0242932bba64c6dec0a84cdc3f1"
     else
-      url "https://github.com/kdbhalala/avdslim/releases/download/v1.0.11/avdslim_v1.0.11_darwin_amd64.tar.gz"
-      sha256 "eff036a466130d3aaba7a1d68122b8c9c8b1d31040fc8284638112389c9aad5a"
+      url "https://github.com/kdbhalala/avdslim/releases/download/v1.0.12/avdslim_v1.0.12_darwin_amd64.tar.gz"
+      sha256 "50a61ba4d5baff9ce35f46ef28e6f962ef24ac2df144c95db945d8b94304fd31"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/kdbhalala/avdslim/releases/download/v1.0.11/avdslim_v1.0.11_linux_arm64.tar.gz"
-      sha256 "5d723f9f690ac74c3678113e040fecb28007f9864a16e4b52dcb81f929354b5e"
+      url "https://github.com/kdbhalala/avdslim/releases/download/v1.0.12/avdslim_v1.0.12_linux_arm64.tar.gz"
+      sha256 "7aa82b0483d4bcca5fc6ab5155ba43f184611e9a825d5d81d304cc4128d78beb"
     else
-      url "https://github.com/kdbhalala/avdslim/releases/download/v1.0.11/avdslim_v1.0.11_linux_amd64.tar.gz"
-      sha256 "9f696b99cc07a9b3935742ac429e37d898842d6da82ec9350e4ba63efbf636da"
+      url "https://github.com/kdbhalala/avdslim/releases/download/v1.0.12/avdslim_v1.0.12_linux_amd64.tar.gz"
+      sha256 "a9a78120adab7f25009a3ffa18e755eacaff7f51205f014561c87e72ca5172bc"
     end
   end
 
